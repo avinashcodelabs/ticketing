6e639eab73bd1a74d8d2afa8fe4d4b0286253e46
avinashcodelabs <avinashcodelabs@gmail.com>
2025-10-20 20:59:36 +0530

    Dockerized, k8s and added skaffold to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 
+app.get("/api/users/signup", (req, res) => {
+    res.send('Hi')
+})
 
 app.listen(3000, () => {
     console.log('auth service running on 3000')
