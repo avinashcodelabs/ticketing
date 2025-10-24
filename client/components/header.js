aa2cc73bbcd7424d5098aa8ecab8bd47b3eddaab
avinashcodelabs <avinashcodelabs@gmail.com>
2025-10-24 18:36:27 +0530

    Bugs

--- client/components/header.js
+++ client/components/header.js
@@ -11,7 +11,7 @@ const Header = ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href={"/"} className="navbar-brand">
-        Ticketing
+        ticketing
       </Link>
       <div className="d-flex justify-content-end">
         <ul className="nav d-flex align-items-center">
